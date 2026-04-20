963dc32bbf3c22adbb0b7611ab4294ba7f22f6a9
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2026-04-20 18:14:21 +0200

    Add integrated datapack

--- src/main/java/de/diruptio/luckyblock/LuckyBlockBootstrap.java
+++ src/main/java/de/diruptio/luckyblock/LuckyBlockBootstrap.java
@@ -1,7 +1,10 @@
 package de.diruptio.luckyblock;
 
+import io.papermc.paper.datapack.DatapackRegistrar;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.registrar.RegistrarEvent;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import io.papermc.paper.registry.RegistryBuilder;
 import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.data.BlockRegistryEntry;
@@ -13,15 +16,24 @@ import org.bukkit.block.BlockType;
 import org.bukkit.inventory.ItemType;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 import java.util.function.Consumer;
 
-@SuppressWarnings("UnstableApiUsage")
+@SuppressWarnings({"unused", "UnstableApiUsage"})
 public final class LuckyBlockBootstrap implements PluginBootstrap {
+    private Logger logger;
+
     @Override
     public void bootstrap(@NonNull BootstrapContext context) {
+        logger = context.getLogger();
         context.getLifecycleManager().registerEventHandler(RegistryEvents.BLOCK.compose(), this::registerBlocks);
         context.getLifecycleManager().registerEventHandler(RegistryEvents.ITEM.compose(), this::registerItems);
+        context.getLifecycleManager().registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY, this::discoverDatapacks);
     }
 
     private void registerBlocks(@NotNull RegistryComposeEvent<BlockType, BlockRegistryEntry.Builder> event) {
@@ -30,6 +42,15 @@ public final class LuckyBlockBootstrap implements PluginBootstrap {
 
     private void registerItems(@NotNull RegistryComposeEvent<ItemType, ItemRegistryEntry.Builder> event) {
         register(event, LuckyBlock.KEY, LuckyBlock.ITEM_BUILDER);
+    }
+
+    private void discoverDatapacks(@NotNull RegistrarEvent<DatapackRegistrar> event) {
+        try {
+            URI uri = Objects.requireNonNull(getClass().getResource("/datapack")).toURI();
+            event.registrar().discoverPack(uri, "provided");
+        } catch (URISyntaxException | IOException e) {
+            logger.error("Failed to register datapack", e);
+        }
     }
 
     private <T, B extends RegistryBuilder<T>> void register(
